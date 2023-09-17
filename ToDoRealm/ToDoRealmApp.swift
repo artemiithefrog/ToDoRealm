25974891ea613c562240545da5cafd0faf7935aa
Artemii <artemiithefrog95@icloud.com>
2023-09-17 14:40:16 +0200

    Initial Commit

---
+++ ToDoRealm/ToDoRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoRealmApp.swift
+//  ToDoRealm
+//
+//  Created by artemiithefrog . on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
